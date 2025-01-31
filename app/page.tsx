843f31b71f6f6a9573884f0ff19c5ae67962a20c
NotMalek <malekanes213@gmail.com>
2025-01-31 14:55:37 -0500

    Added terminal button

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,14 @@ import About from "@/components/about";
 import Experience from "@/components/experience";
 import Intro from "@/components/intro";
 import Projects from "@/components/projects";
-import SectionDivider from "@/components/section-divider";
 import Skills from "@/components/skills";
+import { TerminalButton } from "@/components/terminal-button";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
       <Intro />
-      <SectionDivider />
+      <TerminalButton />
       <About />
       <Projects />
       <Experience />
---
+++ components/terminal-button.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Link from 'next/link';
+
+export const TerminalButton = () => {
+    return (
+        <div className="mt-14 mb-28 flex justify-center">
+            <a
+                // href="https://terminal.malek.dev" to replace
+                target="_blank"
+                rel="noopener noreferrer"
+                className="group relative inline-block"
+            >
+                <div className="absolute -inset-1 rounded-lg bg-gradient-to-r from-[#00ff00] to-[#00cc00] opacity-40 blur transition duration-200 group-hover:opacity-75" />
+                <button
+                    className="relative rounded-lg bg-black px-8 py-4 font-mono text-lg text-[#00ff00] transition duration-200
+                     hover:bg-zinc-900 dark:bg-zinc-900 dark:hover:bg-black"
+                >
+                    {'>'} DON'T CLICK ME {'<'}
+                </button>
+            </a>
+        </div>
+    );
+};
